04d167d382c33c48552a15e988293eb7c1bb87db
Robin Hugues <robin.hugues1@gmail.com>
2022-12-30 12:42:49 +0100

    feat: added the logger config and few refactors/fix for the reports

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -10,14 +10,14 @@ import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 
 public class App {
-    private static final Logger logger = LogManager.getLogger("App");
+	private static final Logger logger = LogManager.getLogger("App");
 
-    public static void main(String[] args) {
-        logger.info("Initializing Parking System");
-        final InputReaderUtil inputReaderUtil = new InputReaderUtil();
-        final ParkingSpotDAO parkingSpotDAO = new ParkingSpotDAO();
-        final TicketDAO ticketDAO = new TicketDAO();
-        final ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        InteractiveShell.loadInterface(inputReaderUtil, parkingService);
-    }
+	public static void main(String[] args) {
+		logger.info("Initializing Parking System");
+		final InputReaderUtil inputReaderUtil = new InputReaderUtil();
+		final ParkingSpotDAO parkingSpotDAO = new ParkingSpotDAO();
+		final TicketDAO ticketDAO = new TicketDAO();
+		final ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+		InteractiveShell.loadInterface(inputReaderUtil, parkingService);
+	}
 }
--- src/main/java/com/parkit/parkingsystem/model/Ticket.java
+++ src/main/java/com/parkit/parkingsystem/model/Ticket.java
@@ -8,103 +8,103 @@ import java.util.Date;
 
 public class Ticket {
 
-    private static final double DISCOUNT = 0.95;
+	private static final double DISCOUNT = 0.95;
 
-    private int id;
+	private int id;
 
-    private ParkingSpot parkingSpot;
+	private ParkingSpot parkingSpot;
 
-    private String vehicleRegNumber;
+	private String vehicleRegNumber;
 
-    private double price;
+	private double price;
 
-    private Date inTime;
+	private Date inTime;
 
-    private Date outTime;
+	private Date outTime;
 
-    private boolean isRecurringUser;
+	private boolean isRecurringUser;
 
-    public Ticket() {
-        this.inTime = new Date();
-    }
+	public Ticket() {
+		this.inTime = new Date();
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public ParkingSpot getParkingSpot() {
-        return parkingSpot;
-    }
+	public ParkingSpot getParkingSpot() {
+		return parkingSpot;
+	}
 
-    public void setParkingSpot(ParkingSpot parkingSpot) {
-        this.parkingSpot = parkingSpot;
-    }
+	public void setParkingSpot(ParkingSpot parkingSpot) {
+		this.parkingSpot = parkingSpot;
+	}
 
-    public String getVehicleRegNumber() {
-        return vehicleRegNumber;
-    }
+	public String getVehicleRegNumber() {
+		return vehicleRegNumber;
+	}
 
-    public void setVehicleRegNumber(String vehicleRegNumber) {
-        this.vehicleRegNumber = vehicleRegNumber;
-    }
+	public void setVehicleRegNumber(String vehicleRegNumber) {
+		this.vehicleRegNumber = vehicleRegNumber;
+	}
 
-    public double getPrice() {
-        return price;
-    }
+	public double getPrice() {
+		return price;
+	}
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
-    public Date getInTime() {
-        return inTime;
-    }
+	public Date getInTime() {
+		return inTime;
+	}
 
-    public void setInTime(Date inTime) {
-        this.inTime = inTime;
-    }
+	public void setInTime(Date inTime) {
+		this.inTime = inTime;
+	}
 
-    public Date getOutTime() {
-        return outTime;
-    }
+	public Date getOutTime() {
+		return outTime;
+	}
 
-    public void setOutTime(Date outTime) {
-        this.outTime = outTime;
-    }
+	public void setOutTime(Date outTime) {
+		this.outTime = outTime;
+	}
 
-    public boolean isRecurringUser() {
-        return isRecurringUser;
-    }
+	public boolean isRecurringUser() {
+		return isRecurringUser;
+	}
 
-    public void setRecurringUser(boolean isRecurringUser) {
-        this.isRecurringUser = isRecurringUser;
-    }
+	public void setRecurringUser(boolean isRecurringUser) {
+		this.isRecurringUser = isRecurringUser;
+	}
 
-    public void computePrice(double ratePerHour) {
-        final Duration duration = calculateDuration();
-        if (isLessThan30MinutesParking(duration)) {
-            this.setPrice(0);
-        } else {
-            final double standardPrice = duration.toMinutes() * ratePerHour / 60;
-            this.setPrice(this.isRecurringUser ? standardPrice * DISCOUNT : standardPrice);
-        }
-    }
+	public void computePrice(double ratePerHour) {
+		final Duration duration = calculateDuration();
+		if (isLessThan30MinutesParking(duration)) {
+			this.setPrice(0);
+		} else {
+			final double standardPrice = duration.toMinutes() * ratePerHour / 60;
+			this.setPrice(this.isRecurringUser ? standardPrice * DISCOUNT : standardPrice);
+		}
+	}
 
-    private boolean isLessThan30MinutesParking(Duration duration) {
-        return duration.toMinutes() < 30;
-    }
+	private boolean isLessThan30MinutesParking(Duration duration) {
+		return duration.toMinutes() < 30;
+	}
 
-    private Duration calculateDuration() {
-        final LocalDateTime start = this.inTime.toInstant().atZone(systemDefault()).toLocalDateTime();
-        final LocalDateTime end = this.outTime.toInstant().atZone(systemDefault()).toLocalDateTime();
-        return Duration.between(start, end);
-    }
+	private Duration calculateDuration() {
+		final LocalDateTime start = this.inTime.toInstant().atZone(systemDefault()).toLocalDateTime();
+		final LocalDateTime end = this.outTime.toInstant().atZone(systemDefault()).toLocalDateTime();
+		return Duration.between(start, end);
+	}
 
-    public boolean isInvalid() {
-        return this.outTime == null || this.outTime.before(this.inTime);
-    }
+	public boolean isInvalid() {
+		return this.outTime == null || this.outTime.before(this.inTime);
+	}
 }
